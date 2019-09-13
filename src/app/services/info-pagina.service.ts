00a3c80155c93663399240f0b410017af8b867d5
susanamg <susana.marquez@atos.net>
2019-09-13 09:56:57 +0200

    Fin seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,39 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
-
     // console.log('Servicio de infoPagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
 
-    // Leer el archivo JSON
+  // Cargar información desde archivo JSON
+  // Leer el archivo JSON: \src\assets\data\data-pagina.json
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
         .subscribe( (resp: InfoPagina) => {
           this.cargada = true;
           this.info = resp;
-          console.log(resp);
+          // console.log(resp);
           // console.log(resp['twitter']);
         });
-
   }
+
+  // Cargar información desde BBDD Firebase
+  private cargarEquipo() {
+    this.http.get('https://angular-templates-1eb08.firebaseio.com/equipo.json')
+        .subscribe( (resp: any[]) => {
+          this.equipo = resp;
+          console.log(resp);
+          // console.log(resp['nombre']);
+        });
+  }
+
+
+
+
+
+
 }

9c602df9b6220899ce3fa214b855f1a3c4d8ddf6
susanamg <susana.marquez@atos.net>
2019-09-13 11:37:53 +0200

    Fin seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -35,8 +35,13 @@ export class InfoPaginaService {
     this.http.get('https://angular-templates-1eb08.firebaseio.com/equipo.json')
         .subscribe( (resp: any[]) => {
           this.equipo = resp;
-          console.log(resp);
-          // console.log(resp['nombre']);
+          // console.log(resp);
+          // console.log(resp[].url);
+          /*
+          for (let persona of this.equipo) {
+            console.log(persona['nombre']);
+          }
+          */
         });
   }
 
